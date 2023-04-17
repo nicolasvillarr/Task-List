80447ee04013a2a834759fc6c7a61a4fbf98917d
nicolasvillarr <nicolasvillar4@gmail.com>
2023-04-17 19:30:15 -0300

    add reactHookForm

--- src/app/newtask/page.js
+++ src/app/newtask/page.js
@@ -3,64 +3,54 @@ import { useTasks } from '@/context/TaskContext'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
-
+import { useForm } from "react-hook-form"
 function Page({params}) {
 
   const router = useRouter()  
-  const [tarea, setTarea] = useState({
-    title:"",
-    desc:""
-  })
   const { tasks, createTask, updateTask } = useTasks()
+  const { register, handleSubmit,setValue, formState:{errors}} = useForm()
 
 
-  
-  function handleChange(e) {
-    setTarea({...tarea, [e.target.name]: e.target.value})
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    if (!tarea.title || !tarea.desc) {
-      if (!tarea) return alert("both data are missing")
-      if(!tarea.title){
-        alert("a title is missing")
-      } else (
-        alert ("a description is missing")
-      )
+  const onSubmit = handleSubmit((task)=>{
+    if (params.id) {
+      updateTask(params.id, task)
     } else {
-      if (params.id) {
-        console.log("edit")
-        updateTask(params.id, tarea)
-      } else {
-        createTask(tarea.title, tarea.desc)
-      }
-      router.push("/")
+      createTask(task.title, task.desc)
     }
-  }
+    router.push("/")
+   }) 
 
   useEffect(()=> {
     if (params.id) {
       const encontreTarea = tasks.find(t => t.id === params.id)
-      if(encontreTarea) setTarea({
-        title: encontreTarea.title, 
-        desc: encontreTarea.desc        
-      })
+      if(encontreTarea) {
+        setValue("title", encontreTarea.title)
+        setValue("desc", encontreTarea.desc)
+      }
     }
   }, [])
   
   return (
     <div>
-    <Link href={"/"}>
-      <h1>Go to home</h1>
-    </Link>
-      <form onSubmit={handleSubmit}>
-        <input onChange={handleChange} value={tarea.title} name='title' placeholder='write a title'/>
-          <textarea onChange={handleChange} value={tarea.desc} name='desc' placeholder='write a task'/>
-          <button>Save</button>
+      <Link href={"/"}>
+        <h1>Go to home</h1>
+      </Link>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="write a title"
+          {...register("title", {required: true})}
+        />
+        {errors.title && (<span>this filed is required</span> )}
+        <textarea
+          placeholder="write a task"
+          {...register("desc", {required: true})}
+        />
+        {errors.desc && (<span>this filed is required</span> )}
+
+        <button>Save</button>
       </form>
     </div>
-  )
+  );
 }
 
 export default Page
